c30721282f5d4811100f73ee8386bbf8002017a9
Julien Caramazza <julien.caramazza@gmail.com>
2019-01-30 16:51:33 +0100

    manque plus que autoclicker

--- script.js
+++ script.js
@@ -12,4 +12,35 @@ import "./style.scss";
   Put the JavaScript code you want below.
 */
 
-console.log("Hey look in your browser console. It works!");
+import cookimg from './PerfectCookie.png';
+let counter=0;
+let multi=50;
+let imulti=1;
+let auto=100;
+
+document.querySelector(".cookie").addEventListener("click",()=>{
+  counter=counter+(1*imulti);
+  document.querySelector(".counter").innerText= counter;
+});
+document.querySelector("#multib").addEventListener("click",()=>{
+  if (counter>=multi){
+    counter=counter-multi;
+      document.querySelector(".counter").innerText= counter;
+    imulti=imulti*2;
+    multi=multi*1.5;
+    document.querySelector("#multib").innerText = "Multiplicateur (" + multi +")";
+
+  }
+
+});
+document.querySelector("#autoclick").addEventListener("click",()=>{
+  if (counter>=auto){
+    counter=counter-auto;
+    document.querySelector(".counter").innerText= counter;
+    auto=auto*1.5;
+
+
+  document.querySelector("#autoclick").innerText="Autoclicker ("+ auto +")";
+
+  }
+})
