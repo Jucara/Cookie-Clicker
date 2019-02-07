d1728b1caa0fc5b806ebe4d19263f8153603830c
Julien Caramazza <julien.caramazza@gmail.com>
2019-02-07 14:46:12 +0100

    Fin du cookie

--- script.js
+++ script.js
@@ -17,30 +17,34 @@ let counter=0;
 let multi=50;
 let imulti=1;
 let auto=100;
+let i=0;
 
 document.querySelector(".cookie").addEventListener("click",()=>{
   counter=counter+(1*imulti);
-  document.querySelector(".counter").innerText= counter;
+  document.querySelector(".counter").innerText= Math.floor(counter);
 });
 document.querySelector("#multib").addEventListener("click",()=>{
   if (counter>=multi){
     counter=counter-multi;
-      document.querySelector(".counter").innerText= counter;
+      document.querySelector(".counter").innerText= Math.floor(counter);
     imulti=imulti*2;
     multi=multi*1.5;
-    document.querySelector("#multib").innerText = "Multiplicateur (" + multi +")";
+    document.querySelector("#multib").innerText = "Multiplicateur (" + Math.floor(multi) +")";
 
   }
 
 });
 document.querySelector("#autoclick").addEventListener("click",()=>{
   if (counter>=auto){
-    counter=counter-auto;
-    document.querySelector(".counter").innerText= counter;
-    auto=auto*1.5;
-
-
-  document.querySelector("#autoclick").innerText="Autoclicker ("+ auto +")";
-
+    i++;
+    counter -= auto;
+    document.querySelector(".counter").innerText= Math.floor(counter);
+    auto *= 1.5;
+    setInterval(()=>increment(),100);
+    document.querySelector("#autoclick").innerText="Autoclicker ("+ Math.floor(auto) +")";
   }
-})
+});
+function increment(){
+  counter+=1;
+  document.querySelector(".counter").innerText=Math.floor(counter);
+}
